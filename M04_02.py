8deab3e0bbd3b3b2907bac847251df9202f218f6
m3rsky <127028296+m3rsky@users.noreply.github.com>
2024-06-25 23:25:32 +0200

    Update M04_02.py
    comments added

--- M04_02.py
+++ M04_02.py
@@ -1,13 +1,39 @@
+'''
+Importowanie modułów
+
+requests do wykonywania zapytań HTTP do API.
+sys do obsługi argumentów wiersza poleceń.
+dateutil.parser do parsowania dat w różnych formatach.
+
+'''
+
 import requests
 import sys
 import dateutil.parser
-from datetime import datetime
 
 
+# 'get_currency_value' Pobiera kurs wymiany waluty z API NBP.
 def get_currency_value(currency, date):
+    """
+    Pobiera kurs wymiany waluty z API Narodowego Banku Polskiego (NBP) dla podanej waluty i daty.
+
+    Args:
+        currency (str): Kod waluty w formacie trzyliterowym (np. 'USD' dla dolara amerykańskiego).
+        date (str): Data w formacie 'YYYY-MM-DD'.
+
+    Returns:
+        float: Kurs wymiany waluty w formacie liczby zmiennoprzecinkowej.
+        None: Jeśli nie uda się pobrać danych z API.
+
+    Example:
+        exchange_rate = get_currency_value('USD', '2023-12-20')
+        if exchange_rate is not None:
+            print(f'Kurs USD z dnia 2023-12-20 to: {exchange_rate}')
+        else:
+            print("Błąd przy pobieraniu danych z API NBP!")
+    """
     url = f'https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{date}/?format=json'
     response = requests.get(url)
-
     if response.status_code == 200:
         data = response.json()
         exchange = data['rates'][0]['mid']
@@ -16,7 +42,20 @@ def get_currency_value(currency, date):
         return None
 
 
+# 'get_user_input' Pobiera dane od użytkownika w formacie waluta data.
 def get_user_input(prompt):
+    """
+    Pobiera dane od użytkownika w formacie waluta data.
+
+    Args:
+        prompt (str): Tekst wyświetlany użytkownikowi jako instrukcja.
+
+    Returns:
+        tuple: Krotka zawierająca dwa elementy: waluta (str) i data (str).
+
+    Example:
+        currency, date_str = get_user_input('Podaj Walutę i datę (np. USD 2023-12-20):\n')
+    """
     while True:
         input_txt = input(prompt)
         parts = input_txt.split()
@@ -26,7 +65,25 @@ def get_user_input(prompt):
             return parts[0], parts[1]
 
 
+# 'parse_date' Parsuje podaną datę do formatu YYYY-MM-DD.
 def parse_date(date_str):
+    """
+    Parsuje podaną datę do formatu 'YYYY-MM-DD'.
+
+    Args:
+        date_str (str): Data w formacie tekstowym.
+
+    Returns:
+        str: Data w formacie 'YYYY-MM-DD'.
+        None: Jeśli format daty jest nieprawidłowy.
+
+    Example:
+        date = parse_date('2023-12-20')
+        if date is not None:
+            print(f'Przekonwertowana data: {date}')
+        else:
+            print("Nieprawidłowy format daty!")
+    """
     try:
         date = dateutil.parser.parse(date_str).strftime('%Y-%m-%d')
         return date
@@ -34,12 +91,24 @@ def parse_date(date_str):
         return None
 
 
+# 'get_exchange_rate' Sprawdza i przetwarza datę, a następnie pobiera kurs waluty.
 def get_exchange_rate(currency, date_str):
+    """
+    Sprawdza poprawność formatu daty i pobiera kurs wymiany waluty.
+
+    Args:
+        currency (str): Kod waluty w formacie trzyliterowym.
+        date_str (str): Data w formacie tekstowym.
+
+    Returns:
+        None
+    """
     date = parse_date(date_str)
     if date is None:
         print("Nieprawidłowy format daty!")
-        currency, date_str = get_user_input(f'Podaj poprawny format daty (np. {currency} 2023-12-20):\n')
-        date = parse_date(date_str)
+        while date is None:
+            currency, date_str = get_user_input(f'Podaj poprawny format daty (np. {currency} 2023-12-20):\n')
+            date = parse_date(date_str)
         if date is None:
             print("Ponownie nieprawidłowy format daty. Zakończenie programu.")
             sys.exit(1)
@@ -52,6 +121,12 @@ def get_exchange_rate(currency, date_str):
 
 
 def main():
+    """
+    Główna funkcja programu, która obsługuje argumenty wiersza poleceń i wywołuje odpowiednie funkcje.
+
+    Returns:
+        None
+    """
     try:
         currency = sys.argv[1]
         date_str = sys.argv[2]
